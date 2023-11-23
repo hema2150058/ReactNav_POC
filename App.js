0b612a3279f0fc07b49553faa73998d94a54f858
hema2150058 <hemapriyavarkala@gmail.com>
2023-11-24 00:54:47 +0530

    third commit

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 
-import { StyleSheet,Text, View,Image } from 'react-native';
+import { StyleSheet} from 'react-native';
 import LoginMpinScreen from './src/screens/LoginMpinScreen';
 import MyTabs from './src/navigators/navigation'
 
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons,Ionicons,Feather,FontAwesome } from '@expo/vector-icons';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import {createDrawerNavigator, DrawerContentScrollView,DrawerItemList} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 //Import native-stack navigator like this
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { TouchableOpacity } from 'react-native';
 
 //Create Native-Stack Navigator like this
 const Stack = createNativeStackNavigator();
@@ -45,14 +43,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-// const HomeScreenStack = () => {
-//   return (
-//     <Stack.Navigator
-//       initialRouteName="HomeScreen"
-//       screenOptions={{headerShown: false}}>
-//       <Stack.Screen name="BottomTabStack" component={MyTabs} />
-//     </Stack.Navigator>
-//   );
-// };
+
 
 export default App;
